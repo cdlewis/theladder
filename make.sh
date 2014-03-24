13c48b9028124aac5201965d030b7c1088442bf0
Christopher Lewis <chris@chris-laptop.local>
2014-03-25 09:25:59 +1100

    moar tracey and makefiles

---
+++ make.sh
@@ -0,0 +1,2 @@
+yes | rm index.html
+python parse_results.py FootyTipping2014.xlsx ladder.html > index.html
--- parse_results.py
+++ parse_results.py
@@ -30,7 +30,7 @@ wb = load_workbook( spreadsheet_file )
 ws = wb.get_sheet_by_name( wb.get_sheet_names()[ 0 ] )
 
 heading_filter = set( [ "PLAYER" ] + [ i for i in xrange( 1, 24 ) ] )
-player_filter = set( [ "Chris Lewis", "Sarah Nicholson", "Daniel Bevan", "Kelly O'Dwyer", "Andrew Danos", "James Doolan", "Lachlan McNaughton", "Michael Lewis", "TigerLilyBet", "Adrian Maher", "Emma Nicholson", "Courtney VanTongeren", "Sarah Casey", "Tom O'Dwyer", "Tracy Embleton" ] )
+player_filter = set( [ "Chris Lewis", "Sarah Nicholson", "Daniel Bevan", "Kelly O'Dwyer", "Andrew Danos", "James Doolan", "Lachlan McNaughton", "Michael Lewis", "TigerLilyBet", "Adrian Maher", "Emma Nicholson", "Courtney VanTongeren", "Sarah Casey", "Tom O'Dwyer", "Tracey Embleton" ] )
 
 headings = OrderedDict( [ ( i.value, i ) for i in ws.rows[ 0 ] if i.value in heading_filter ] )
 players = OrderedDict( [ ( i[ 0 ].value, i[ 0 ].address.replace( 'A', '' ) ) for i in ws.rows[ 1: ] if i[ 0 ].value in player_filter ] )
