0e68a624cb1ed974636bca4088f851d0c3d2cc7c
Christopher Lewis <chris@chris-laptop.local>
2014-04-01 10:11:18 +1100

    got ranks backwards /doh

--- parse_results.py
+++ parse_results.py
@@ -25,6 +25,7 @@ players_file = sys.argv[ 2 ]
 template_file = sys.argv[ 3 ]
 
 # Abort if required files does not exist
+
 for file in [ spreadsheet_file, players_file, template_file ]:
 	if not os.path.isfile( file ):
 		print "%s not found, exiting..." % file
@@ -36,6 +37,7 @@ ws = wb.get_sheet_by_name( wb.get_sheet_names()[ 0 ] )
 heading_filter = set( [ "PLAYER" ] + [ i for i in xrange( 1, 24 ) ] )
 
 # Process player file
+
 raw_players = open( players_file, "r" ).read().split( "\n" )
 player_filter = set()
 zero_players = set()
@@ -53,6 +55,7 @@ players = OrderedDict( [ ( i[ 0 ].value, i[ 0 ].address.replace( 'A', '' ) ) for
 player_vectors = [ [ headings[ j ].offset( row=( int( players[ i ] ) - 1 ) ).value for j in headings ] for i in players ]
 
 # Account for zero players
+
 for player in player_vectors:
 	if player[ 0 ] not in zero_players:
 		continue
@@ -62,13 +65,14 @@ for player in player_vectors:
 
 player_vectors.sort( key=lambda x: -1 * sum( [ i for i in x[ 1: ] if i is not None ] ) ) # sort to correctly position zero players
 
-# Cut player vectors to remove NoneTypes
+# Cut player/header vectors to remove NoneTypes
+
 cut = sum( [ 1 for i in player_vectors[0] if i != None ] )
 player_vectors = [ i[ 0:cut ] for i in player_vectors ]
 headings = headings.keys()[ 1:cut ]
 
-
 # Augment player vectors to include explicit rank
+
 ladder = [ { 'rank': i, \
              'name': player_vectors[ i ][ 0 ], \
              'total': sum( player_vectors[ i ][ 1: ] ), \
@@ -76,9 +80,17 @@ ladder = [ { 'rank': i, \
 } for i in xrange( 0, len( player_vectors ) ) ]
 
 # Calculate last week's rank
-last_week = sorted( [ ( i[ 0 ], sum( i[ 1:-1] ) ) for i in player_vectors ], key=lambda x: -x[1] )
+
+# Create least of (name, total) tuples sorted by name
+last_week = sorted( [ ( i[ 0 ], sum( i[ 1:-1] ) ) for i in player_vectors ], key=lambda x: x[ 0 ] )
+
+# Re-order list by score
+last_week.sort( key=lambda x: -x[ 1 ] )
+
+# Convert to map of name -> rank
 last_week = dict( [ ( last_week[ i ][ 0 ], i ) for i in xrange( 0, len( last_week ) ) ] )
 
 # Render
+
 template = Template( open( template_file, 'r' ).read() )
 print template.render( locals() )
